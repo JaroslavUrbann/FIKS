7b7ed40426fb80efa00b03e94701ff38aa7e59d2
JaroslavUrban <jaroslav.urbann@gmail.com>
2018-10-03 14:14:49 +0200

    + pár algoritmů

--- cenzorky.py
+++ cenzorky.py
@@ -1,17 +1,67 @@
-l = []
-for i in range(15):
-    l.append(i)
+import random
+import math
 
-result = []
-al = 0
-for x in l:
-    al ^= x
-    result.append(al)
-    print(al)
+l = random.sample(range(100), 10)
 
-print("-----")
-print(12^13^11)
-print(1^11)
+
+def SearchTable(l, mintable, maxtable, parity, a, b):
+    o = b - a + 1
+    k = math.floor(math.log(o, 2))
+    moveby = o - 2 ** k
+    if l[maxtable[k][a]] > l[maxtable[k][a + moveby]]:
+        return min(l[mintable[k][a]], l[mintable[k][a + moveby]]), maxtable[k][a], parity[a - 1] ^ parity[b]
+    else:
+        return min(l[mintable[k][a]], l[mintable[k][a + moveby]]), maxtable[k][a + moveby], parity[a - 1] ^ parity[b]
+
+
+def CreateTable(l):
+    avariable = True
+    mintable = []
+    maxtable = []
+    parity = [l[0]]
+    countparity = True
+    j = 0
+    x = 0
+    y = 0
+    i = 0
+    while avariable:
+        width = 2**j
+        mintable.append([])
+        maxtable.append([])
+        while i + width < len(l):
+            mini = l.index(min(l[i:i + width]))
+            maxi = l.index(max(l[i:i + width]))
+            mintable[x].append(mini)
+            maxtable[x].append(maxi)
+            if countparity and i < len(l) - 1:
+                parity.append(l[i + 1] ^ parity[i])
+            i += 1
+            y += 1
+        countparity = False
+        x += 1
+        j += 1
+        i = 0
+        y = 0
+        if width + width > len(l):
+            avariable = False
+    return mintable, maxtable, parity
+
+print(l)
+mintable, maxtable, parity = CreateTable(l)
+print(parity)
+print(l[1]^l[0])
+print(parity[0]^parity[1])
+print(SearchTable(l, mintable, maxtable, parity, 0, 2))
+# result = []
+# al = 0
+# for x in l:
+#     al ^= x
+#     result.append(al)
+    # print(al)
+
+# print("-----")
+# print(12^13^11)
+# print(1^11)
 # XOR done
 # Range minimum query
 # asi taky
